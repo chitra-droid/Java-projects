3bb8800c549081ceeb254b8c61dfe254ef47107e
chitra-droid <ucb9460@gmail.com>
2024-07-18 10:20:13 +0530

    Add Get Doctor Api by id and Get Doctor Api by specialisation

--- Vaccino/src/main/java/com/example/Vaccino/controller/DoctorController.java
+++ Vaccino/src/main/java/com/example/Vaccino/controller/DoctorController.java
@@ -1,12 +1,12 @@
 package com.example.Vaccino.controller;
 
+import com.example.Vaccino.Enum.Specialization;
 import com.example.Vaccino.Model.Doctor;
 import com.example.Vaccino.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctor")
@@ -24,5 +24,15 @@ public class DoctorController {
         catch (Exception e){
            return "Invalid Doctor request!";
         }
+    }
+
+    @GetMapping("/get")
+    public Doctor getDoctor(@RequestParam int id){
+       return doctorService.getDoctor(id);
+    }
+
+    @GetMapping("/getBySpecialization")
+    public List<Doctor> getDoctor(@RequestParam Specialization specialization){
+        return doctorService.getDoctor(specialization);
     }
 }
--- Vaccino/src/main/java/com/example/Vaccino/service/DoctorService.java
+++ Vaccino/src/main/java/com/example/Vaccino/service/DoctorService.java
@@ -1,9 +1,14 @@
 package com.example.Vaccino.service;
 
+import com.example.Vaccino.Enum.Specialization;
 import com.example.Vaccino.Model.Doctor;
 import com.example.Vaccino.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DoctorService {
@@ -15,5 +20,33 @@ public class DoctorService {
 
         Doctor savedDoc = doctorRepository.save(doctor);
         return savedDoc;
+    }
+
+
+    public Doctor getDoctor(int id) {
+        Optional<Doctor> OptionalDoc =  doctorRepository.findById(id);
+        if(OptionalDoc.isEmpty()){
+            throw new RuntimeException("Invalid Id");
+        }
+        Doctor doc = OptionalDoc.get();
+        return doc;
+    }
+
+    public List<Doctor> getDoctor(Specialization specialization) {
+        List<Doctor> Doctors = doctorRepository.findAll();
+
+        List<Doctor> res = new ArrayList<>();
+
+        for(Doctor doc : Doctors){
+            if(doc.getSpecialization().equals(specialization)){
+                res.add(doc);
+            }
+        }
+        if(res.size()==0){
+            throw new RuntimeException("Doctor not found");
+        }
+
+        return res;
+
     }
 }
